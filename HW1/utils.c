a98205d63be910c202d95d0041ff83d4a42f7975
Jesse Hunt <jhhunt@g.cofc.edu>
2016-01-19 21:29:42 -0500

    Initial upload of HW1

---
+++ HW1/utils.c
@@ -0,0 +1,18 @@
+// -----------------------------------
+// CSCI 340 - Operating Systems I 
+// Spring 2015 (Tony Leclerc, Brent Munsell)
+// utils.c implementation file
+// Homework Assignment 1
+//
+// -----------------------------------
+
+// ----------------------------------------------
+// These are the only libraries that can be 
+// used. Under no circumstances can additional 
+// libraries be included
+#include <stdio.h>	// IO functions
+#include <stdlib.h> 	// atof function
+#include <math.h>	// math functions
+#include "utils.h"	// your own functions
+
+// Now you put your implementation of the function prototypes here...
