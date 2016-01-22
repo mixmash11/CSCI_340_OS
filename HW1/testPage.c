47ae819747b56da6e6d42e7c835dc546b097681c
Jesse Hunt <jhhunt@g.cofc.edu>
2016-01-22 00:05:11 -0500

    Most of read method working.

--- HW1/testPage.c
+++ HW1/testPage.c
@@ -14,8 +14,15 @@ main (int argc, char** argv)
   char line_String[40];
   char radius_String[8];
   char theta_String[8];
-  int iterator;
+  int string_iterator;
   int theta_iterator;
+  int vector_iterator;
+  double radius_in;
+  double theta_in;
+  v_struct *vector_array;
+
+  vector_array = (v_struct*) malloc (sizeof (v_struct));
+  vector_iterator = 0;
 
   //  BEGIN CODE 
   //  CHECK FILE WAS PASSED
@@ -34,10 +41,10 @@ main (int argc, char** argv)
   printf ("Reading file...\n");
 
   //  READ LOOP
-  fgets (line_String, 40, vectorFile);
-  while (vectorFile != NULL)
+  while (!feof (vectorFile))
     {
-      printf ("String:\n     %s", line_String);
+      fgets (line_String, 40, vectorFile);
+      //      printf ("String:\n     %s", line_String);
 
       //    iterator = 0;
       //    while(line_String[iterator] != '\0'){
@@ -46,41 +53,80 @@ main (int argc, char** argv)
       //      }
 
       //  READ LINE INTO RADIUS VALUE
-      printf ("     Reading radius...\n");
-      iterator = 0;
-      while (line_String[iterator] != ',')
+      //      printf ("     Reading radius...\n");
+      string_iterator = 0;
+      while (line_String[string_iterator] != ',')
         {
 
-          printf ("Character %i: %c\n", iterator, line_String[iterator]);
+          //          printf ("Character %i: %c\n", iterator, line_String[iterator]);
 
-          radius_String[iterator] = line_String[iterator];
-          iterator++;
+          radius_String[string_iterator] = line_String[string_iterator];
+          string_iterator++;
         }
-      radius_String[iterator] = '\0'; // CLOSE RADIUS STRING
-      iterator++; // MOVES TO CHARACTER AFTER COMMA
+      radius_String[string_iterator] = '\0'; // CLOSE RADIUS STRING
+      string_iterator++; // MOVES TO CHARACTER AFTER COMMA
 
       //  READ LINE INTO THETA VALUE
-      printf ("     Reading theta...\n");
+      //      printf ("     Reading theta...\n");
       theta_iterator = 0;
-      while (line_String[iterator] != '\0')
+      while (line_String[string_iterator] != '\0')
         {
 
-          printf ("Character %i: %c\n", iterator, line_String[iterator]);
+          //          printf ("Character %i: %c\n", iterator, line_String[iterator]);
 
-          theta_String[theta_iterator] = line_String[iterator];
-          iterator++;
+          theta_String[theta_iterator] = line_String[string_iterator];
+          string_iterator++;
           theta_iterator++;
         }
       theta_String[theta_iterator] = '\0'; // CLOSE RADIUS STRING
 
-      printf ("Radius is: %s\n", radius_String);
-      printf ("Theta is: %s\n", theta_String);
-      
-      fgets (line_String, 40, vectorFile);
+      //      printf ("Radius is: %s\n", radius_String);
+      //      printf ("Theta is: %s\n", theta_String);
+
+      //  CONVERT VALUES TO FLOATS
+      radius_in = atof (radius_String);
+      theta_in = atof (theta_String);
+
+      printf ("Radius is: %.2f, ", radius_in);
+      printf ("Theta is: %.2f\n", theta_in);
+
+      //  CHECK/CORRECT THETA VALUES
+      while (theta_in > 360)
+        {
+          printf ("     Theta TOO HIGH: %.2f\n", theta_in);
+
+          theta_in = theta_in - 360;
+
+          printf ("Theta is: %.2f\n", theta_in);
+        }
+      while (theta_in < 0)
+        {
+          printf ("     Theta TOO LOW: %.2f\n", theta_in);
+
+          theta_in = theta_in + 360;
+
+          printf ("Theta is: %.2f\n", theta_in);
+        }
+
+      //  ENTER VALUES INTO VECTOR ARRAY
+      vector_array[0].r = radius_in;
+      vector_array[0].theta = theta_in;
+
+      vector_iterator++;
+
     }
 
-
-
+  //    while (vector_iterator >= 0){
+  //        printf ("Vector %i radius is: %.2f\n", vector_iterator, radius_in);
+  //        printf ("Vector %i theta is: %.2f\n", vector_iterator, theta_in);
+  //        
+  //        vector_iterator = vector_iterator - 1;
+  //      }
+  
+  printf("Vector count is %i\n", vector_iterator);
+  
+  printf ("Vector %i radius is: %.2f\n", 0, radius_in);
+  printf ("Vector %i theta is: %.2f\n", 0, theta_in);
 
   printf ("Closing file...\n");
   fclose (vectorFile);
